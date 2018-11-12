662c2f348dacb2a234d07524e9ee5dae9fcc14aa
seanwestfall <sean.westfall@gmail.com>
2018-11-12 01:33:42 -0800

    added the Map Sprite component and also the todo mvc example app

--- src/Components/Map.purs
+++ src/Components/Map.purs
@@ -2,6 +2,17 @@ module Components.Map where
 
 import Prelude
 
+import Control.Monad.Eff (Eff)
+import Data.Foldable (fold)
+
 import React.DOM as R
 import React.DOM.Props as RP
 import Thermite as T
+
+main :: Eff (dom :: DOM) Unit
+main =
+    render $ fold
+	[ h1 (text "Try PureScript!")
+	      , p (text "Try out the examples below, or create your own!")
+	]
+  where
